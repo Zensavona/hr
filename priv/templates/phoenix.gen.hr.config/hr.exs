3cfe1b9b2b06264a05c1555412c87c8248ed8afd
Zen Savona <z@zensavona.com>
2015-10-06 15:19:50 +0200

    eex generated stuff

---
+++ priv/templates/phoenix.gen.hr.config/hr.exs
@@ -0,0 +1,10 @@
+use Mix.Config
+
+config :hr, locale: :en,
+            model: <%= module %>,
+            logged_in_url: "/",
+            not_logged_in_url: "/error",
+            logged_out_url: "/",
+            signed_up_url: "/",
+            register_from_email: "Registration <welcome@yourawesomeapp.com>",
+            password_recovery_from_email: "Password Recovery <no-reply@yourawesomeapp.com>"
---
+++ priv/templates/phoenix.gen.hr.config/migration.exs
@@ -0,0 +1,23 @@
+defmodule <%= base %>.Repo.Migrations.CreateIdentity do
+  use Ecto.Migration
+
+  def change do
+    create table(:identities) do
+      add :owner_id, references(:<%= plural %>)
+      add :provider, :string, null: false
+      add :access_token, :string, null: false
+      add :refresh_token, :string
+      add :uid, :string
+      add :email, :string
+      add :nickname
+      add :image
+      add :phone
+      add :first_name
+      add :last_name
+
+      timestamps
+    end
+    create index(:<%= plural %>, [:owner_id])
+
+  end
+end
