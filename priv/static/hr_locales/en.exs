990ee5b5106fea6126c599cf73589026237b7835
Zen Savona <z@zensavona.com>
2015-10-12 16:24:45 +0200

    use file for locale messages

---
+++ priv/static/hr_locales/en.exs
@@ -0,0 +1,21 @@
+[
+  sessions: [
+    signed_in: "Signed in successfully.",
+    signed_out: "Signed out successfully.",
+    invalid: "Invalid email or password."
+  ],
+  registrations: [
+    signed_up: "Welcome! You have signed up successfully.",
+    signed_up_but_unconfirmed: "A message with a confirmation link has been sent to %{email}. Please follow the link to activate your account."
+  ],
+  confirmations: [
+    confirmed: "Your email address has been successfully confirmed.",
+    invalid_confirmation_token: "The token you provided was already used, or invalid."
+  ],
+  passwords: [
+    send_instructions: "You will receive an email with instructions on how to reset your password in a few minutes.",
+    updated: "Your password has been changed successfully.",
+    no_token: "You can't access this page without coming from a password reset email. If you do come from a password reset email, please make sure you used the full URL provided.",
+    invalid: "Invalid password."
+  ]
+]
