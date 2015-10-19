c92a47887d1c4beae0138b567989ebfe0cae3f96
Zen Savona <z@zensavona.com>
2015-10-19 15:46:12 +0200

    remacrofy

--- lib/hr/mail_helper.ex
+++ lib/hr/mail_helper.ex
@@ -1,16 +1,22 @@
 defmodule Hr.BaseMailHelper do
   defmacro __using__(dir) do
     quote do
-      use Mailgun.Client, domain: Application.get_env(:hr, :mailgun_domain),
-                         key: Application.get_env(:hr, :mailgun_key),
-                         mode: Application.get_env(:hr, :mailgun_mode),
-                         test_file_path: Application.get_env(:hr, :mailgun_test_file_path)
+      use Mailgun.Client
+
+      def conf do
+        [
+          domain: Application.get_env(:hr, :mailgun_domain),
+          key: Application.get_env(:hr, :mailgun_key),
+          mode: Application.get_env(:hr, :mailgun_mode),
+          test_file_path: Application.get_env(:hr, :mailgun_test_file_path)
+        ]
+      end
 
       def send_confirmation_email(user, link) do
         send_email to: user.unconfirmed_email,
                    from: Application.get_env(:hr, :register_from_email),
                    subject: Application.get_env(:hr, :confirmation_email_subject),
-                   html: EEx.eval_file(unquote(dir) <> "signup_confirmation.eex", [user: user, link: link])
+                   html: EEx.eval_file(@dir <> "signup_confirmation.eex", [user: user, link: link])
       end
 
       def send_reset_email(user, link) do
