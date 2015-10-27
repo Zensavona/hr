5a5dc73bf6904b359ef45808506805fadfc3443f
Zen Savona <z@zensavona.com>
2015-10-27 18:50:50 +0100

    use secret_key_base and tidy up a bit

--- lib/hr/plug.ex
+++ lib/hr/plug.ex
@@ -4,7 +4,7 @@ defmodule Hr.Plug do
 
   def hr_for(conn, entity) do
     conn = put_private(conn, :hr_entity, entity)
-    {entity, model, repo, app} = Hr.Meta.stuff(conn)
+    {entity, model, repo, _} = Hr.Meta.stuff(conn)
 
 
     user_id = get_session(conn, :"hr_#{entity}_id")
@@ -14,23 +14,19 @@ defmodule Hr.Plug do
 
   def hr_jwt_for(conn, entity) do
     conn = put_private(conn, :hr_entity, entity)
-    {entity, model, repo, app} = Hr.Meta.stuff(conn)
+    {entity, model, repo, _} = Hr.Meta.stuff(conn)
 
     case get_req_header(conn, "authorization") do
       [auth] ->
         # |> with_validation("user_id", &(&1 == 21)) ?
-        auth = auth |> token |> with_signer(hs256("my_secret")) |> verify
+        auth = auth |> token |> with_signer(hs256(conn.secret_key_base)) |> verify
 
         user = case auth do
           %{error: nil} ->
-            IO.inspect "OK, cool ============"
             repo.get_by(model, [id: auth.claims["id"], email: auth.claims["email"]])
           _ ->
-            IO.inspect "INCORRECT"
             ""
         end
-        IO.inspect user
-
         assign(conn, :"current_#{entity}", user)
       _ ->
         conn
