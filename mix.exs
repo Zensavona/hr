d1cb10ada8999a2f7c8cba2427409f4338ad6dc1
Zen Savona <z@zensavona.com>
2015-10-28 01:54:39 +0100

    bump v

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Hr.Mixfile do
 
   def project do
     [app: :hr,
-     version: "0.2.1",
+     version: "0.2.2",
      elixir: "~> 1.0",
      compilers: [:phoenix] ++ Mix.compilers,
      test_coverage: [tool: ExCoveralls],
